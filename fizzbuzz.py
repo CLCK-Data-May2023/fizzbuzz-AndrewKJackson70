9c16abccce5b700e1fa01d1efa2b2ee3c1fbded2
Andrew Jackson <andrewkjackson@gmail.com>
2023-07-20 18:31:34 -0400

    Added code to fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,12 @@
 # add your code here
-
+#FizzBuzz Challenge. Written by Andrew Jackson
+number_range = 1
+while number_range <= 100:
+    print(number_range, end=" ")
+    number_range += 1
+    if number_range % 3 == 0 and number_range % 5 == 0:
+        print("FizzBuzz")
+    elif number_range % 3 == 0:
+        print("Fizz") 
+    elif number_range % 5 == 0:
+        print("Buzz")
