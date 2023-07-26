973bc854977329d6485c685ac24a677e2057b3c5
Andrew Jackson <andrewkjackson@gmail.com>
2023-07-26 15:10:15 -0400

    adjusted range to include the number 100

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
 # add your code here
 #FizzBuzz Challenge. Written by Andrew Jackson
-for num in range(1,100):
+for num in range(1,101):
     string = ""
     if num % 3 == 0 and num % 5 == 0:
         string = string + "FizzBuzz"
@@ -8,5 +8,7 @@ for num in range(1,100):
         string = string + "Fizz"
     elif num % 5 == 0:
         string = string + "Buzz"
+     elif num % 5 != 0 and num % 3 != 0:
+        string = string + str(num)
     print(string)   
    
