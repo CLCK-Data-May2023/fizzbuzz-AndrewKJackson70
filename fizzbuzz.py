7bcc1b45dd261f33d84333ccb42292352fb5c798
Andrew Jackson <andrewkjackson@gmail.com>
2023-07-26 14:47:20 -0400

    modified fizzbuzz.py to display proper output

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,14 @@
 # add your code here
 #FizzBuzz Challenge. Written by Andrew Jackson
-number_range = 1
-while number_range <= 100:
-    print(number_range, end=" ")
-    number_range += 1
-    if number_range % 3 == 0 and number_range % 5 == 0:
-        print("FizzBuzz")
-    elif number_range % 3 == 0:
-        print("Fizz") 
-    elif number_range % 5 == 0:
-        print("Buzz")
+for num in range(1,100):
+    string = ""
+    if num % 3 == 0 and num % 5 == 0:
+        string = string + "FizzBuzz"
+    elif num % 3 == 0:
+        string = string + "Fizz"
+    elif num % 5 == 0:
+        string = string + "Buzz"
+    elif num % 5 != 0 and num % 3 != 0:
+        string = string + str(num)
+    print(string)   
+   
